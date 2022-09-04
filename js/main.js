4a360cbbca3d42099e536dbedd1c717a744cce3f
Rokas <rookas.rudzenskas@gmail.com>
2022-09-04 11:04:54 +0300

    Rokas

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const scene = new THREE.Scene();
+// Red cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// camera
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+scene.add(camera);
